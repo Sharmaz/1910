f69da7f217705d246c6ac11977deed3372ea124e
Sharmaz <irae45@gmail.com>
2015-11-18 14:45:54 -0600

    Manejo de rutas y sirviendo ficheros, landing page simple

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 'use strict'
 
+const fs = require('fs')
 const http = require('http')
+const path = require('path')
+const st = require('st')
+
+
 const port = process.env.PORT || 8080
-const server = http.createServer(function (req, res) {
-	res.end('1910 Studios')
+
+const server = http.createServer()
+
+server.on('request', onRequest)
+server.on('listening', onListening)
+
+server.listen(port)
+
+const mount = st({
+	path: path.join(__dirname, '.', 'public'),
+	index: 'index.html'
 })
 
-server.listen(port, function () {
-	console.log('Server running in port' + port)
-})
+function onRequest (req, res) {
+	mount(req, res, function(err) {
+		if (err) return res.end(err.message)
+
+		res.statusCode = 404
+		res.end(`Not Found ${req.url}`)
+	})
+}
+
+function onListening() {
+	console.log(`Servidor escuchando en el puerto: ${port}`)
+}
