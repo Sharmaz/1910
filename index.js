a0aad686fdd435ef5638a6bca2d35cd5258ab3e0
Sharmaz <irae45@gmail.com>
2016-01-13 23:55:27 -0600

    Cambiando la estructura del site y utilizando el modulo express

--- index.js
+++ index.js
@@ -4,30 +4,20 @@ const fs = require('fs')
 const http = require('http')
 const path = require('path')
 const st = require('st')
-
+const express = require('express')
+const web = express()
+var server
 
 const port = process.env.PORT || 8080
 
-const server = http.createServer()
+server = web.listen(port, onListening())
 
-server.on('request', onRequest)
-server.on('listening', onListening)
+web.use('/static', express.static('static'))
+web.use('/en', express.static('/public/en/template.html'))
 
-server.listen(port)
-
-const mount = st({
-	path: path.join(__dirname, '.', 'public'),
-	index: 'index.html'
+web.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html')
 })
-
-function onRequest (req, res) {
-	mount(req, res, function(err) {
-		if (err) return res.end(err.message)
-
-		res.statusCode = 404
-		res.end(`Not Found ${req.url}`)
-	})
-}
 
 function onListening() {
 	console.log(`Servidor escuchando en el puerto: ${port}`)
