a48bd87cd402a9136b5e7de90ed7dbb42688a361
Sharmaz <irae45@gmail.com>
2016-01-21 23:20:50 -0600

    Formulario de contacto procesado en el backend

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path')
 const st = require('st')
 const express = require('express')
 const web = express()
+const nodemailer = require('nodemailer')
+const mg = require('nodemailer-mailgun-transport')
+const parser = require('body-parser')
+
 var server
 
 const port = process.env.PORT || 8080
@@ -28,6 +32,38 @@ web.get('/es/template.html', function(req, res) {
 	res.sendFile(__dirname + '/public/es/template.html')
 })
 
+web.use( parser.urlencoded({ extended: true }))
+web.post('/enviar', function(req, res) {
+	var auth = {
+  auth: {
+    api_key: 'key-4cec4805d46b7c935457f2c405b43f5f',
+    domain: '1910-studios.com'
+  }
+}
+	var nombre = req.body.nombre
+	var mail = req.body.mail
+	var mensaje = req.body.mensaje
+	var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+	nodemailerMailgun.sendMail({
+	  from: mail,
+	  to: 'irae45@gmail.com', // An array if you have multiple recipients.
+	  subject: '1910 Formulario',
+	  text: 'Hey, ' + nombre + ' Tiene un mensaje: ' + mensaje
+	}, function (err, info) {
+	  if (err) {
+	  	res.status(500).json({
+	  		error: 'No fue posible enviar en estos momentos :('
+	  	})
+	    console.log('Error: ' + err);
+	  }
+	  else {
+	  	res.status(200).json({
+	  		message: 'Listo, mensaje enviado :)'
+	  	})
+	    console.log('Response: ' + info);
+	  }
+	});
+})
 
 function onListening() {
 	console.log(`Servidor escuchando en el puerto: ${port}`)
